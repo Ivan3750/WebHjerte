4bd2eb4f8cc2418344eaa9c819eb5fbce5a15786
Ivan Kohan <kohan3750@gmail.com>
2025-04-02 20:45:08 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,21 +3,60 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'webhjerte.dk',
+        protocol: "https",
+        hostname: "webhjerte.dk", // або інший конкретний домен для зображень
       },
     ],
   },
+  reactStrictMode: true,
+  crossOrigin: "anonymous",
 
+  // Переписування URL
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://api.adzuna.com/v1/api/jobs/us/search/1/:path*",
+      },
+    ];
+  },
+
+  // Налаштування заголовків безпеки та CORS
   async headers() {
     return [
       {
-        source: "/api/:path*", 
+        source: "/:path*",
         headers: [
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET, POST, PUT, DELETE, OPTIONS" },
-          { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization" },
-          { key: "Access-Control-Allow-Credentials", value: "true" }, 
+          {
+            key: "Content-Security-Policy",
+            value: `
+              default-src 'self';
+              script-src 'self' 'nonce-randomValue' 'strict-dynamic';
+              style-src 'self' 'unsafe-inline';
+              img-src 'self' data: https:;
+              font-src 'self' data:;
+              connect-src 'self' https:;
+              base-uri 'self';
+              form-action 'self';
+              frame-ancestors 'none';
+            `.replace(/\s{2,}/g, " ").trim(),
+          },
+          {
+            key: "Access-Control-Allow-Origin", 
+            value: "*" // Або визначте конкретні домени, наприклад, https://example.com
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS"
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization"
+          },
+          {
+            key: "Access-Control-Allow-Credentials",
+            value: "true"
+          },
         ],
       },
     ];
